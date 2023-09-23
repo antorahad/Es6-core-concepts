4c92fb99a3002c0397067110589cdb4b74636460
antorahad <antorahad6@gmail.com>
2023-09-23 17:54:47 +0600

    destructuring in api

--- api/app.js
+++ api/app.js
@@ -8,20 +8,21 @@ const loadData = () => {
 const displayData = data => {
     const productContainer = document.getElementById('product-container');
     data.forEach(product => {
+        const {pname, brand, color, ram, hardDisk, ssd, price, inStock} = product;
         const innerDiv = document.createElement('div');
         innerDiv.classList.add('product-card')
         innerDiv.innerHTML=  `
         <div>
         <img src='https://images.unsplash.com/photo-1603302576837-37561b2e2302?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGxhcHRvcHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80' alt=''></img>
         </div>
-        <h1>${product.pname}</h1>
-        <p>Brand: ${product.brand}</p>
-        <p>Color: ${product.color}</p>
-        <p>Ram: ${product.ram}</p>
-        <p>Harddisk: ${product.hardDisk}</p>
-        <p>SSD: ${product.ssd ? product.ssd : 'No'}</p>
-        <p>Price: ${product.price}</p>
-        <p>InStock: ${product.inStock ? 'Yes' : 'No'}</p>
+        <h1>${pname}</h1>
+        <p>Brand: ${brand}</p>
+        <p>Color: ${color}</p>
+        <p>Ram: ${ram}</p>
+        <p>Harddisk: ${hardDisk}</p>
+        <p>SSD: ${ssd ? ssd : 'No'}</p>
+        <p>Price: ${price}</p>
+        <p>InStock: ${inStock ? 'Yes' : 'No'}</p>
         <div>
             <button>Add Cart</button>
         </div>
